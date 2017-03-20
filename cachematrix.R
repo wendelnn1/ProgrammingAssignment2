867a678cb646657a8f5cafe146532edb171406dc
wendelnn1 <nicholas.wendeln@gmail.com>
2017-03-20 19:03:53 -0400

    Changed from mean to inverse matrix
    Programming Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cahematrix.R computes the invers of a square matrix using cache designators to avoid potentially time-consuming computations.
 
-## Write a short comment describing this function
+
+## 1) Sets the value of the matrix, 2) gets the value of matrix, 3) sets the value of the inverse, 4)gets the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m<-NULL
+    set<-function(y){
+        x<<-y
+        m<<-NULL
+    }
+    get<-function()x
+    setinv<-function(inverse) m<<-inverse
+    getinv<-function()m
+    list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
 
 
-## Write a short comment describing this function
-
+##calculates inverse of matrix set by makeCacheMatrix function and will get if already been found
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m<-x$getinv()
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+data<-x$get()
+m<-solve(data,...)
+x$setinv(m)
+m
 }
